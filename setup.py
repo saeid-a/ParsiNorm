bf29e661a72bf76d295aeffd2e37d4296b619af1
Saeid Aghapour <a@a.com>
2024-03-06 11:14:50 +0330

    rename directory

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='parsinorm-fork',  
-     version='0.0.2',
-     packages=['parsinorm-fork'] ,
-     author="HaraAi",
-     author_email="info@hara.ai",
-     description="Persain Text Pre-Proceesing Tool",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/haraai/ParsiNorm",
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
-     install_requires=['num2fawords==1.1', 'persian-tools==0.0.10', 'urlextract==1.4.0', 'nltk==3.3', 'hazm==0.7.0'],
- )
+    name='parsinorm-fork-fork',
+    version='0.0.2',
+    packages=['parsinorm-fork-fork'],
+    author="HaraAi",
+    author_email="info@hara.ai",
+    description="Persain Text Pre-Proceesing Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/saeid-a/ParsiNorm",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['num2fawords==1.1', 'persian-tools==0.0.10', 'urlextract==1.4.0', 'nltk>=3.6.5', 'hazm==0.7.0'],
+)
