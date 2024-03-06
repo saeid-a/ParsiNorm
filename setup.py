b9133012839eace4476e0c83a646da4c0579f119
Saeid Aghapour <a@a.com>
2024-03-06 11:25:49 +0330

    bump hazm version

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['num2fawords==1.1', 'persian-tools==0.0.10', 'urlextract==1.4.0', 'nltk>=3.6.5', 'hazm==0.7.0'],
+    install_requires=['num2fawords==1.1', 'persian-tools==0.0.10', 'urlextract==1.4.0', 'nltk>=3.6.5', 'hazm>=0.7.0'],
 )
